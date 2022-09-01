8578b1b8ba5896d6a92d4f598faeaf5a8e94f63f
Anatoliy Makaveyev <Anatoliy.Makaveyev@ibm.com>
2022-09-01 12:17:15 +0200

    data import

--- bankruptcy_analysis.py
+++ bankruptcy_analysis.py
@@ -1,1 +1,7 @@
-print("first repository test")
+
+import pandas as pd
+import numpy as np
+from scipy.io import arff
+
+dataset = arff.loadarff(open(r'/Users/anatoliymakaveyev/Documents/Python Environment/BANKRUPTCY_PREDICTION_CASE/data/1year.arff'))
+df_data = pd.DataFrame(dataset[0])
